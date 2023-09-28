bf4f2a1f0445396f6ae99b790a91fab038a6df57
Lara-Simonetti <l.simonetti@uniandes.edu.co>
2023-09-28 01:02:51 -0500

    Vistas Movil y primera versión proyecto

---
+++ AndroidMaqueta/app/src/main/java/com/ux/mooble/Advertencia.java
@@ -0,0 +1,14 @@
+package com.ux.mooble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Advertencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertencia);
+    }
+}
